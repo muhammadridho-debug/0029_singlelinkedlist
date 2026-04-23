45da73f1685d77abaab4945b33b18821dbb3c47b
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-23 16:01:23 +0700

    implementasi addNode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -19,6 +19,31 @@ public:
         START = NULL;
     }
     
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL || nim == START->noMhs)
+            {
+               cout << "\nDuplikasi noMhs tidak diijinkan\n";
+               return;
+            }
+            
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+        Node *previous = START;
+        Node *current = START;
+
+
     bool listEmpty()
     {
         return START == NULL;
