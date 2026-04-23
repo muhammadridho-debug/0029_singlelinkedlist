16c51f0037e2ee1c1b4eb9329866ea412a9d9d67
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-23 16:05:54 +0700

    menambahkan fungsi delNode untuk menghapus data mahasiswa

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -76,7 +76,22 @@ public:
         return (current != NULL);
     }
 
-    
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search (nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+        delete current;
+        return true;
+    }
+
     void traverse()
     {
         if (listEmpty())
