450ca396c2f47ed033944fbec9695ae8562c925b
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-23 15:55:38 +0700

    membuat struktur class Node dan LinkedList

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -2,6 +2,25 @@
 #include <string>
 using namespace std;
 
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+};
+
+
 int main()
 {
     return 0;
