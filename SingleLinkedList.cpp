d5e9fb86203f9428d3273819d08ba8eb8c0e348f
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-23 16:04:51 +0700

    implementasi fungsi search untuk mencari node spesifik

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -62,6 +62,21 @@ public:
         return START == NULL;
     }
 
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    
     void traverse()
     {
         if (listEmpty())
